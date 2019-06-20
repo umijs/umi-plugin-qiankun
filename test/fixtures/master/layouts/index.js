0028cfb7980bff378dbc9b7e1fc98342514bf0aa
Kuitos <kuitos.lau@gmail.com>
2019-06-20 18:25:39 +0800

    :tada: integrate with qiankun

--- test/fixtures/master/layouts/index.js
+++ test/fixtures/master/layouts/index.js
@@ -7,8 +7,8 @@ export default function() {
       <header>
         <ul>
           <li><Link to="/">Home</Link></li>
-          <li><Link to="/app1">App1</Link></li>
-          <li><Link to="/app2">App2</Link></li>
+          <li><Link to="#/app1">App1</Link></li>
+          <li><Link to="#/app2">App2</Link></li>
         </ul>
       </header>
       <div id="app-root" style={{ padding: 10, background: '#ccc' }}></div>
