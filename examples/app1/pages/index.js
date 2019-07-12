9ed17dc8c78e190ead690e0586b0445f0b02b207
Kuitos <kuitos.lau@gmail.com>
2019-07-12 14:40:00 +0800

    chore(package.json): add examples scripts

--- examples/app1/pages/index.js
+++ examples/app1/pages/index.js
@@ -1,10 +1,10 @@
 import { useRootExports } from 'umi';
-import styles from './index.css';
+import styles from './index.less';
 
 export default function() {
   const rootExports = useRootExports();
   return (
-    <div className={styles.normal}>
+    <div className={`${styles.normal} normalTest`}>
       <h1>App 1 </h1>
       <p>data: {JSON.stringify(rootExports.getData())}</p>
     </div>
