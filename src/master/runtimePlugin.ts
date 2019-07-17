b932d7a935839c50b9da5dcbc0fa13e210423104
Kuitos <kuitos.lau@gmail.com>
2019-07-17 20:30:05 +0800

    feat(master): 子应用的 history mode 跟主应用一致时，自动生成相应的路由配置

--- src/master/runtimePlugin.ts
+++ src/master/runtimePlugin.ts
@@ -3,24 +3,34 @@ import subAppConfig from '@tmp/subAppsConfig.json';
 import { registerMicroApps, start } from 'qiankun';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { defaultMountContainerId, noop } from '../common';
+import { IConfig } from 'umi-types';
+import { defaultHistoryMode, defaultMountContainerId, noop } from '../common';
+import { Options } from '../types';
 
 export function render(oldRender: typeof noop) {
   oldRender();
 
-  function isAppActive(location: Location, routerBase: string) {
-    // TODO 支持 browserHistory， 根据当前配置的 history 类型决定
-    return location.hash.startsWith(routerBase);
+  function isAppActive(location: Location, history: IConfig['history'], base: IConfig['base'] = '/') {
+    switch (history) {
+      case 'hash':
+        return location.hash.startsWith(`#${base}`);
+
+      case 'browser':
+        return location.pathname.startsWith(base);
+
+      default:
+        return false;
+    }
   }
 
-  const { apps, jsSandbox = false, prefetch = true } = subAppConfig;
+  const { apps, jsSandbox = false, prefetch = true } = subAppConfig as Options;
   registerMicroApps(
-    apps.map(({ name, entry, routerBase, mountElementId = defaultMountContainerId, ...props }) => {
+    apps.map(({ name, entry, base, history = defaultHistoryMode, mountElementId = defaultMountContainerId, ...props }) => {
 
       return {
         name,
         entry,
-        activeRule: location => isAppActive(location, routerBase),
+        activeRule: location => isAppActive(location, history, base),
         render: ({ appContent, loading }) => {
           if (process.env.NODE_ENV === 'development') {
             console.info(`app ${name} loading ${loading}`);
