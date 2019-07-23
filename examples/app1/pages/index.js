5dc5ace6d03a1503df713ebddb1d6535e2e62039
Kuitos <kuitos.lau@gmail.com>
2019-07-23 18:15:47 +0800

    :memo: update example

--- examples/app1/pages/index.js
+++ examples/app1/pages/index.js
@@ -1,4 +1,6 @@
 import { useRootExports } from 'umi';
+import Link from 'umi/link';
+
 import styles from './index.less';
 
 export default function() {
@@ -7,6 +9,7 @@ export default function() {
     <div className={`${styles.normal} normalTest`}>
       <h1>App 1 </h1>
       <p>data: {JSON.stringify(rootExports.getData())}</p>
+      <Link to="/nest">nest page</Link>
     </div>
   );
 }
