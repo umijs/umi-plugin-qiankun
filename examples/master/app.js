71507f457fab251e9e0898e3781d40456914c446
Kuitos <kuitos.lau@gmail.com>
2019-08-20 17:00:50 +0800

    :bug: use mergeAsyncConfig api instead

--- examples/master/app.js
+++ examples/master/app.js
@@ -1,19 +1,23 @@
 //import './global.css'
 
 window.isMain = true;
-export const qiankun = {
-  apps: [
-    {
-      name: 'app1',
-      entry: 'http://localhost:8001/app1',
-      base: '/app1',
-      mountElementId: 'root-slave',
-    },
-    {
-      name: 'app2',
-      entry: 'http://localhost:8002/app2',
-      base: '/app2',
-      mountElementId: 'root-slave',
-    },
-  ],
-};
+export const qiankun = new Promise(resolve => {
+  setTimeout(() => {
+    resolve({
+      apps: [
+        {
+          name: 'app1',
+          entry: 'http://localhost:8001/app1',
+          base: '/app1',
+          mountElementId: 'root-slave',
+        },
+        {
+          name: 'app2',
+          entry: 'http://localhost:8002/app2',
+          base: '/app2',
+          mountElementId: 'root-slave',
+        },
+      ],
+    });
+  }, 1000);
+});
