ffe2b9366dfdb12657c2ed247a59b8bc82a56a5e
三直 <i@sanzhi.me>
2019-08-23 15:26:28 +0800

    :bug: fix:async get config (#21)

--- examples/master/app.js
+++ examples/master/app.js
@@ -1,23 +1,35 @@
-//import './global.css'
-
+// 标识位，方便子应用单独调试
 window.isMain = true;
+
+// 异步demo
 export const qiankun = new Promise(resolve => {
   setTimeout(() => {
-    resolve({
-      apps: [
-        {
-          name: 'app1',
-          entry: 'http://localhost:8001/app1',
-          base: '/app1',
-          mountElementId: 'root-slave',
-        },
-        {
-          name: 'app2',
-          entry: 'http://localhost:8002/app2',
-          base: '/app2',
-          mountElementId: 'root-slave',
-        },
-      ],
-    });
-  }, 1000);
+    window.g_app._store
+      .dispatch({
+        type: 'base/getApps',
+      })
+      .then(apps => {
+        resolve({
+          apps,
+        });
+      });
+  }, 0);
 });
+
+// 同步demo
+// export const qiankun = {
+//   apps: [
+//     {
+//       name: 'app1',
+//       entry: 'http://localhost:8001/app1',
+//       base: '/app1',
+//       mountElementId: 'root-slave',
+//     },
+//     {
+//       name: 'app2',
+//       entry: 'http://localhost:8002/app2',
+//       base: '/app2',
+//       mountElementId: 'root-slave',
+//     },
+//   ],
+// }
---
+++ examples/master/mock/app.js
@@ -0,0 +1,16 @@
+export default {
+  'GET /api/apps': [
+    {
+      name: 'app1',
+      entry: 'http://localhost:8001/app1',
+      base: '/app1',
+      mountElementId: 'root-slave',
+    },
+    {
+      name: 'app2',
+      entry: 'http://localhost:8002/app2',
+      base: '/app2',
+      mountElementId: 'root-slave',
+    },
+  ],
+};
