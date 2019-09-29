b8552bf9f0f41d986b3b39e488b0225caa3b2ca5
howel52 <401738000@qq.com>
2019-09-29 19:19:39 +0800

    :art: add prettier and formatted

--- src/types.ts
+++ src/types.ts
@@ -8,7 +8,7 @@ import IConfig from 'umi-types/config';
 
 export type App = {
   name: string;
-  entry: string | { scripts: string[], styles: string[] };
+  entry: string | { scripts: string[]; styles: string[] };
   base: string | string[];
   props?: object;
 } & Pick<IConfig, 'history' | 'mountElementId'>;
@@ -19,4 +19,4 @@ export type Options = {
   prefetch: boolean;
   defer?: boolean;
   lifeCycles?: LifeCycles<object>;
-}
+};
