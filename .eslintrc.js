796bbb47ccdb63337794ed8f0f0947149b6e5840
Kuitos <kuitos.lau@gmail.com>
2019-10-08 18:51:18 +0800

    :rotating_light: add lib dir to prettier ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 0,
     'no-return-assign': 0,
     'no-console': 0,
-    'max-len': ['error', { code: 120 }],
+    'max-len': ['error', { code: 120, ignoreComments: true }],
     // see https://github.com/prettier/prettier/issues/3847
     'space-before-function-paren': ['error', { anonymous: 'never', named: 'never', asyncArrow: 'always' }],
   },
