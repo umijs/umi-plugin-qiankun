c52010a42eb0ed1a1a3dabf8062e883b07dd4fc9
Kuitos <kuitos.lau@gmail.com>
2019-10-22 15:17:48 +0800

    ✅ fix the path prefix match bug and add unit test (#36)
    * :bug::white_check_mark: fix the path prefix match bug and add unit test
    
    * :green_heart: add pre-push hook

---
+++ src/__tests__/common.test.ts
@@ -0,0 +1,32 @@
+/**
+ * @author Kuitos
+ * @since 2019-10-22
+ */
+
+import { testPathWithPrefix } from '../common';
+
+it('testPathPrefix', () => {
+  // browser history
+  expect(testPathWithPrefix('/js', '/js')).toBeTruthy();
+  expect(testPathWithPrefix('/js', '/jss')).toBeFalsy();
+  expect(testPathWithPrefix('/js', '/js/')).toBeTruthy();
+  expect(testPathWithPrefix('/js', '/js/s')).toBeTruthy();
+  expect(testPathWithPrefix('/js', '/js/s/a')).toBeTruthy();
+  expect(testPathWithPrefix('/js', '/js/s?a=b')).toBeTruthy();
+  expect(testPathWithPrefix('/js', '/js?a=b')).toBeTruthy();
+  expect(testPathWithPrefix('/js', '/js?')).toBeTruthy();
+  expect(testPathWithPrefix('/js', '/js/?')).toBeTruthy();
+  expect(testPathWithPrefix('/js', '/js/?a=b')).toBeTruthy();
+
+  // hash history
+  expect(testPathWithPrefix('#/js', '#/js')).toBeTruthy();
+  expect(testPathWithPrefix('#/js', '#/jss')).toBeFalsy();
+  expect(testPathWithPrefix('#/js', '#/js/')).toBeTruthy();
+  expect(testPathWithPrefix('#/js', '#/js/s')).toBeTruthy();
+  expect(testPathWithPrefix('#/js', '#/js/s/a')).toBeTruthy();
+  expect(testPathWithPrefix('#/js', '#/js/s?a=b')).toBeTruthy();
+  expect(testPathWithPrefix('#/js', '#/js?a=b')).toBeTruthy();
+  expect(testPathWithPrefix('#/js', '#/js?')).toBeTruthy();
+  expect(testPathWithPrefix('#/js', '#/js/?')).toBeTruthy();
+  expect(testPathWithPrefix('#/js', '#/js/?a=b')).toBeTruthy();
+});
