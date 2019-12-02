455a9441171abe15936b2dc95c5e07bd01cf7403
Kuitos <kuitos.lau@gmail.com>
2019-12-02 15:35:17 +0800

    :recycle: 优化逻辑

--- src/master/index.ts
+++ src/master/index.ts
@@ -24,41 +24,42 @@ export default function(api: IApi, options: Options) {
   } = api;
   // apps 可能在构建期为空
   const { apps = [] } = options || {};
+  if (apps.length) {
+    const modifyAppRoutes = (masterHistory: IConfig['history']) => {
+      api.modifyRoutes(routes => {
+        const newRoutes = routes.map(route => {
+          if (route.path === '/') {
+            if (route.routes && route.routes.length) {
+              apps.forEach(({ history: slaveHistory = history, base }) => {
+                // 当子应用的 history mode 跟主应用一致时，为避免出现 404 手动为主应用创建一个 path 为 子应用 rule 的空 div 路由组件
+                if (slaveHistory === masterHistory) {
+                  const baseConfig = toArray(base);
+                  baseConfig.forEach(basePath =>
+                    route.routes!.unshift({
+                      path: `${basePath}/(.*)`,
+                      component: `() => {
+                        if (process.env.NODE_ENV === 'development') {
+                          console.log('${basePath} 404 mock rendered');
+                        }
 
-  function modifyAppRoutes(masterHistory: IConfig['history']) {
-    api.modifyRoutes(routes => {
-      const newRoutes = routes.map(route => {
-        if (route.path === '/') {
-          if (route.routes && route.routes.length) {
-            apps.forEach(({ history: slaveHistory = history, base }) => {
-              // 当子应用的 history mode 跟主应用一致时，为避免出现 404 手动为主应用创建一个 path 为 子应用 rule 的空 div 路由组件
-              if (slaveHistory === masterHistory) {
-                const baseConfig = toArray(base);
-                baseConfig.forEach(basePath =>
-                  route.routes!.unshift({
-                    path: `${basePath}/(.*)`,
-                    component: `() => {
-              if (process.env.NODE_ENV === 'development') {
-                console.log('${basePath} 404 mock rendered');
-              }
-              
-              return React.createElement('div');
-            }`,
-                  }),
-                );
-              }
-            });
+                        return React.createElement('div');
+                      }`,
+                    }),
+                  );
+                }
+              });
+            }
           }
-        }
 
-        return route;
+          return route;
+        });
+
+        return newRoutes;
       });
+    };
 
-      return newRoutes;
-    });
+    modifyAppRoutes(history);
   }
-
-  modifyAppRoutes(history);
 
   const rootExportsFile = join(api.paths.absSrcPath, 'rootExports.js');
   api.addPageWatcher(rootExportsFile);
@@ -80,7 +81,7 @@ window.g_rootExports = ${existsSync(rootExportsFile) ? `require('@/rootExports')
   api.writeTmpFile(
     'qiankunDefer.js',
     `
-      class Deferred { 
+      class Deferred {
         constructor() {
           this.promise = new Promise(resolve => this.resolve = resolve);
         }
