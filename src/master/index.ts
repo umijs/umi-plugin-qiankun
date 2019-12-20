4d4a81783a78cb08ef0fc6470790c2f5dd073d9d
Kuitos <kuitos.lau@gmail.com>
2019-12-20 17:48:30 +0800

    :sparkles: 兼容遗留的配置了重名路由的场景

--- src/master/index.ts
+++ src/master/index.ts
@@ -25,46 +25,52 @@ export default function(api: IApi, options: Options) {
   // apps 可能在构建期为空
   const { apps = [] } = options || {};
   if (apps.length) {
-    // 检查路由中是否已存在跟 basePath 一致的路由
-    const isBasePathExist = (routes: IRoute[], basePath: string): boolean =>
-      routes.some(route => {
-        if (route.path && testPathWithPrefix(basePath, route.path)) return true;
+    // 获取一组路由中以 basePath 为前缀的路由
+    const findRouteWithPrefix = (routes: IRoute[], basePath: string): IRoute | null => {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const route of routes) {
+        if (route.path && testPathWithPrefix(basePath, route.path)) return route;
 
         if (route.routes && route.routes.length) {
-          return isBasePathExist(route.routes, basePath);
+          return findRouteWithPrefix(route.routes, basePath);
         }
+      }
 
-        return false;
-      });
+      return null;
+    };
 
     const modifyAppRoutes = (masterHistory: IConfig['history']) => {
       api.modifyRoutes(routes => {
         const newRoutes = routes.map(route => {
-          if (route.path === '/') {
-            if (route.routes && route.routes.length) {
-              apps.forEach(({ history: slaveHistory = history, base }) => {
-                // 当子应用的 history mode 跟主应用一致时，为避免出现 404 手动为主应用创建一个 path 为 子应用 rule 的空 div 路由组件
-                if (slaveHistory === masterHistory) {
-                  const baseConfig = toArray(base);
-                  baseConfig.forEach(basePath => {
-                    // 应用没有自己配置过 basePath 相关路由，则自动加入 mock 的路由
-                    if (!isBasePathExist(routes, basePath)) {
-                      route.routes!.unshift({
-                        path: basePath,
-                        exact: false,
-                        component: `() => {
+          if (route.path === '/' && route.routes && route.routes.length) {
+            apps.forEach(({ history: slaveHistory = history, base }) => {
+              // 当子应用的 history mode 跟主应用一致时，为避免出现 404 手动为主应用创建一个 path 为 子应用 rule 的空 div 路由组件
+              if (slaveHistory === masterHistory) {
+                const baseConfig = toArray(base);
+
+                baseConfig.forEach(basePath => {
+                  const routeWithPrefix = findRouteWithPrefix(routes, basePath);
+
+                  // 应用没有自己配置过 basePath 相关路由，则自动加入 mock 的路由
+                  if (!routeWithPrefix) {
+                    route.routes!.unshift({
+                      path: basePath,
+                      exact: false,
+                      component: `() => {
                         if (process.env.NODE_ENV === 'development') {
                           console.log('${basePath} 404 mock rendered');
                         }
 
                         return React.createElement('div');
                       }`,
-                      });
-                    }
-                  });
-                }
-              });
-            }
+                    });
+                  } else {
+                    // 若用户已配置过跟应用 base 重名的路由，则强制将该路由 exact 设置为 false，目的是兼容之前遗留的错误用法的场景
+                    routeWithPrefix.exact = false;
+                  }
+                });
+              }
+            });
           }
 
           return route;
