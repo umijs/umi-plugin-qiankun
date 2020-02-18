d8f008dcabe8c9dcd82edaec866155c38128d6c2
Kuitos <kuitos.lau@gmail.com>
2020-02-18 11:01:02 +0800

    :art:

--- src/__tests__/common.test.ts
+++ src/__tests__/common.test.ts
@@ -3,8 +3,8 @@
  * @since 2019-10-22
  */
 
-import { testPathWithPrefix, recursiveCoverRouter, addSpecifyPrefixedRoute } from '../common';
-import { originSingleRoute, expectCoverRoute, originRoutes, expectRoutes } from './mock';
+import { addSpecifyPrefixedRoute, testPathWithPrefix } from '../common';
+import { expectRoutes, originRoutes } from './mock';
 
 it('testPathPrefix', () => {
   // browser history
@@ -91,9 +91,6 @@ it('testPathPrefix', () => {
 });
 
 it('testRecursiveCoverRouter', () => {
-  // 路由覆盖单测
-  expect(String(recursiveCoverRouter(originSingleRoute, 'test'))).toEqual(String(expectCoverRoute));
-
   // 在原route的基础上添加指定路由单测
   expect(String(addSpecifyPrefixedRoute(originRoutes, 'test'))).toEqual(String(expectRoutes));
   expect(String(addSpecifyPrefixedRoute(originRoutes, 'test', 'app2'))).toEqual(String(expectRoutes));
